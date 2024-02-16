0be34029c70f1aed1757f8698d6fa06320143ed7
Leslie Graff <leslienoragraff@hotmail.com>
2024-02-16 16:47:22 -0600

    Step 1
    Declaring Variables

--- UnityTempProj/Assets/Scripts/AppleTree.cs
+++ UnityTempProj/Assets/Scripts/AppleTree.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    //prefab for instantiating apples
+    public GameObject applePrefab;
+
+    //Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    //Chance that the apple treee will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    //Rate at which Apples will be instatiated
+    public float secondsBetweeenAppleDrops = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +29,8 @@ public class AppleTree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Basic Movement
+        //Changing Direction
         
     }
 }
